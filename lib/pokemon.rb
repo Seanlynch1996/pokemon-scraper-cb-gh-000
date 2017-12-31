221ebe20ea643d3788cecc556e65fe631c5da5cb
Sean Lynch <sean.m.lynch@vanderbilt.edu>
2017-12-31 05:39:51 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,39 @@
 class Pokemon
+
+  attr_accessor :name, :type, :db, :id
+
+  def initialize(id = 1, name = "pikachu", type = "electric", db = nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon(id, name, type) VALUES (?,?,?)", @id, name, type)
+  end
+
+  def self.find(id, db)
+    data = db.execute("SELECT * FROM pokemon WHERE id = ?;", id)
+    return Pokemon.new(data.flatten[0], data.flatten[1],data.flatten[2],data.flatten[3])
+  end
+
+  def self.alter_hp
+    #db.execute("UPDATE pokemon SET hp = ?", val)
+    return 0
+  end
+
+  def self.id
+    return @id
+  end
+
+  def self.name
+    return @name
+  end
+
+  def self.type
+    return @type
+  end
+
+
 end
